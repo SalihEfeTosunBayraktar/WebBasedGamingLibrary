a9793076ac2716beb3fed2087a05271fef4c787d
Legendnoobe <trozge73@gmail.com>
2026-04-11 01:03:56 +0300

    refactor: move server status notification message to precede process execution in start script

--- start.bat
+++ start.bat
@@ -8,12 +8,13 @@ echo.
 echo Sunucular baslatiliyor...
 
 :: Launch both Node and Vite together
+echo =========================================
+echo    Web Sunucusu Şuan Çalışıyor verilen Adrese Giderek Oyunlarını Yönetebilirsin.
+echo =========================================
 call npx -y concurrently --kill-others --names "BACKEND,FRONTEND" -c "blue,magenta" "cd backend && node index.js" "cd frontend && npm run dev"
 
 echo Sistem basariyla kapatildi.
 
-echo =========================================
-echo    Web Sunucusu Şuan Çalışıyor verilen Adrese Giderek Oyunlarını Yönetebilirsin.
-echo =========================================
+
 
 pause > nul
