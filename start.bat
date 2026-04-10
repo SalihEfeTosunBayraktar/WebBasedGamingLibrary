0081fd1423db89b216b7da35fbee62b8153c8393
Legendnoobe <trozge73@gmail.com>
2026-04-11 01:04:07 +0300

    fix: remove Turkish characters from server status message in start.bat

--- start.bat
+++ start.bat
@@ -9,7 +9,7 @@ echo Sunucular baslatiliyor...
 
 :: Launch both Node and Vite together
 echo =========================================
-echo    Web Sunucusu Şuan Çalışıyor verilen Adrese Giderek Oyunlarını Yönetebilirsin.
+echo    Web Sunucusu Suan Calisiyor verilen Adrese Giderek Oyunlarini Yonetebilirsin.
 echo =========================================
 call npx -y concurrently --kill-others --names "BACKEND,FRONTEND" -c "blue,magenta" "cd backend && node index.js" "cd frontend && npm run dev"
 
